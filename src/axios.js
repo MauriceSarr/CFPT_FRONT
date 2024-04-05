11294ec20d9c5396d5dfa28d72d4d2fa03e53e51
MauriceSarr <mauricesarr2016@gmail.com>
2024-04-05 10:30:32 +0000

    update axios

--- src/axios.js
+++ src/axios.js
@@ -4,4 +4,10 @@ const instance= axios.create({
     baseURL: 'http://18.212.87.32:8000/api/'
 });
 
+axios.interceptors.request.use(function (config) {
+    const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+    config.headers['X-CSRF-TOKEN'] = csrfToken;
+    return config;
+});
+
 export default instance;
